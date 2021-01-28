2a84dc1f5adcc58cfc9395b5ff62d77b5398f044
wangchao <magician.of.technique@gmail.com>
2021-01-28 15:48:16 +0800

    [*] 修改配置名称

--- pkg/framework/security/oauth2/config/oauth2_security_configurer.go
+++ pkg/framework/security/oauth2/config/oauth2_security_configurer.go
@@ -1,24 +1,24 @@
-package security
+package config
 
 import (
 	"github.com/ingot-cloud/ingot-go/pkg/framework/security"
 	"github.com/ingot-cloud/ingot-go/pkg/framework/security/web/config"
 )
 
-// CustomHTTPConfigurer 安全配置
-type CustomHTTPConfigurer struct {
+// OAuth2SecurityConfigurer OAuth2安全配置
+type OAuth2SecurityConfigurer struct {
 }
 
 // Build 构建安全配置
-func (c *CustomHTTPConfigurer) Build() (security.WebSecurityConfigurers, error) {
+func (oa *OAuth2SecurityConfigurer) Build() (security.WebSecurityConfigurers, error) {
 	var configurers security.WebSecurityConfigurers
 	configurers = append(configurers, &config.WebSecurityConfigurerAdapter{
-		AdditionalConfigurer: c,
+		AdditionalConfigurer: oa,
 	})
 	return configurers, nil
 }
 
 // Configure 配置
-func (c *CustomHTTPConfigurer) Configure(http security.HTTPSecurityBuilder) error {
+func (oa *OAuth2SecurityConfigurer) Configure(http security.HTTPSecurityBuilder) error {
 	return nil
 }
