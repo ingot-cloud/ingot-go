c200ea1d717f4a215ac0ac6295f16032ee327001
wangchao <magician.of.technique@gmail.com>
2021-02-01 21:29:29 +0800

    [+] grant type constant

---
+++ pkg/framework/security/oauth2/constants/grant_type.go
@@ -0,0 +1,10 @@
+package constants
+
+// 授权类型
+const (
+	GrantTypePassword = "password"
+	GrantTypeCode     = "authorization_code"
+	GrantTypeImplicit = "implicit"
+	GrantTypeClient   = "client_credentials"
+	GrantTypeRefresh  = "refresh_token"
+)
