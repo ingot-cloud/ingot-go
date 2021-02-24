f7ff3b8c3966fe6172df140217dbf17d72816bc8
wangchao <magician.of.technique@gmail.com>
2021-02-24 11:06:29 +0800

    [*] 修改 OAuth2SecurityConfigurer

--- pkg/framework/security/oauth2/config/oauth2_security_configurer.go
+++ pkg/framework/security/oauth2/config/oauth2_security_configurer.go
@@ -2,26 +2,26 @@ package config
 
 import (
 	"github.com/ingot-cloud/ingot-go/pkg/framework/security"
+	coreAuth "github.com/ingot-cloud/ingot-go/pkg/framework/security/authentication"
 	"github.com/ingot-cloud/ingot-go/pkg/framework/security/oauth2/authentication"
-	"github.com/ingot-cloud/ingot-go/pkg/framework/security/web/config"
 )
 
 // OAuth2SecurityConfigurer OAuth2安全配置
 type OAuth2SecurityConfigurer struct {
+	tokenExtractor        authentication.TokenExtractor
+	authenticationManager coreAuth.Manager
 }
 
-// Build 构建安全配置
-func (oa *OAuth2SecurityConfigurer) Build() (security.WebSecurityConfigurers, error) {
-	var configurers security.WebSecurityConfigurers
-	configurers = append(configurers, &config.WebSecurityConfigurerAdapter{
-		AdditionalConfigurer: oa,
-	})
-	return configurers, nil
+// NewOAuth2SecurityConfigurer 实例化 OAuth2 安全配置
+func NewOAuth2SecurityConfigurer(tokenExtractor authentication.TokenExtractor, authenticationManager coreAuth.Manager) *OAuth2SecurityConfigurer {
+	return &OAuth2SecurityConfigurer{
+		tokenExtractor:        tokenExtractor,
+		authenticationManager: authenticationManager,
+	}
 }
 
 // Configure 配置
 func (oa *OAuth2SecurityConfigurer) Configure(http security.HTTPSecurityBuilder) error {
-	// todo
-	http.AddFilter(authentication.NewOAuth2ProcessingFilter(authentication.NewBearerTokenExtractor(), nil))
+	http.AddFilter(authentication.NewOAuth2ProcessingFilter(oa.tokenExtractor, oa.authenticationManager))
 	return nil
 }
