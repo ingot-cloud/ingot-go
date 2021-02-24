8abea75611cfe81bce1eb1ab71487fa60c66d770
wangchao <magician.of.technique@gmail.com>
2021-02-24 16:32:50 +0800

    [*] 注入默认值

--- internal/app/core/security/configurer.go
+++ internal/app/core/security/configurer.go
@@ -2,23 +2,13 @@ package security
 
 import (
 	"github.com/ingot-cloud/ingot-go/pkg/framework/security"
-	"github.com/ingot-cloud/ingot-go/pkg/framework/security/web/config"
+	"github.com/ingot-cloud/ingot-go/pkg/framework/security/web/container"
 )
 
 // Build 构建安全配置
 func Build() (security.WebSecurityConfigurers, error) {
-	var configurers security.WebSecurityConfigurers
-	configurers = append(configurers, &config.WebSecurityConfigurerAdapter{
-		AdditionalConfigurer: &CustomHTTPConfigurer{},
-	})
+	configurers := container.WebSecurityConfigurers()
+	// 可以追加自定义 WebSecurityConfigurer
+	// todo 追加先后问题
 	return configurers, nil
-}
-
-// CustomHTTPConfigurer 安全配置
-type CustomHTTPConfigurer struct {
-}
-
-// Configure 配置
-func (c *CustomHTTPConfigurer) Configure(http security.HTTPSecurityBuilder) error {
-	return nil
 }
