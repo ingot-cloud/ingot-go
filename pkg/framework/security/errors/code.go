b6b377290315cbac3d8ce3934697b138a4c3e6ad
wangchao <magician.of.technique@gmail.com>
2021-02-26 11:28:12 +0800

    [*] 整理安全错误码

---
+++ pkg/framework/security/errors/code.go
@@ -0,0 +1,10 @@
+package errors
+
+// 账户状态码
+const (
+	AccountLockCode        = "S0001"
+	AccountDisabledCode    = "S0002"
+	AccountExpiredCode     = "S0003"
+	BadCredentialsCode     = "S0004"
+	CredentialsExpiredCode = "S0005"
+)
--- pkg/framework/security/errors/func.go
+++ pkg/framework/security/errors/func.go
@@ -1,6 +1,8 @@
 package errors
 
 import (
+	"net/http"
+
 	"github.com/ingot-cloud/ingot-go/pkg/framework/core/errors"
 	"github.com/ingot-cloud/ingot-go/pkg/framework/core/utils"
 )
@@ -9,4 +11,40 @@ import (
 func ProviderNotFound(args ...string) error {
 	message := utils.StringCombine(args...)
 	return errors.Unauthorized(message)
+}
+
+// UsernameNotFound 用户未找到
+func UsernameNotFound(args ...string) error {
+	message := utils.StringCombine(args...)
+	return errors.New(http.StatusUnauthorized, BadCredentialsCode, message)
+}
+
+// BadCredentials 错误凭证
+func BadCredentials(args ...string) error {
+	message := utils.StringCombine(args...)
+	return errors.New(http.StatusUnauthorized, BadCredentialsCode, message)
+}
+
+// AccountLock 账户已锁定
+func AccountLock(args ...string) error {
+	message := utils.StringCombine(args...)
+	return errors.New(http.StatusUnauthorized, AccountLockCode, message)
+}
+
+// AccountDisabled 账户禁用
+func AccountDisabled(args ...string) error {
+	message := utils.StringCombine(args...)
+	return errors.New(http.StatusUnauthorized, AccountDisabledCode, message)
+}
+
+// AccountExpired 账户过期
+func AccountExpired(args ...string) error {
+	message := utils.StringCombine(args...)
+	return errors.New(http.StatusUnauthorized, AccountExpiredCode, message)
+}
+
+// CredentialsExpired 凭证过期
+func CredentialsExpired(args ...string) error {
+	message := utils.StringCombine(args...)
+	return errors.New(http.StatusUnauthorized, CredentialsExpiredCode, message)
 }
