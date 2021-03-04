03b73807c6c9cecab2a8a88341e5acc6d6cbe2ca
wangchao <magician.of.technique@gmail.com>
2021-03-04 16:17:48 +0800

    [*] 去掉无用api

--- internal/app/container/provider/set/api.go
+++ internal/app/container/provider/set/api.go
@@ -2,17 +2,14 @@ package set
 
 import (
 	"github.com/google/wire"
-	"github.com/ingot-cloud/ingot-go/internal/app/api"
 	"github.com/ingot-cloud/ingot-go/internal/app/core/http"
 
 	coreApi "github.com/ingot-cloud/ingot-go/pkg/framework/core/web/api"
 )
 
 var apiConfigSet = wire.NewSet(wire.Struct(new(http.APIConfig), "*"), wire.Bind(new(coreApi.HTTPConfigurer), new(*http.APIConfig)))
-var apiOAuth2 = wire.NewSet(wire.Struct(new(api.OAuth2), "*"))
 
 // APISet api注入
 var APISet = wire.NewSet(
 	apiConfigSet,
-	apiOAuth2,
 )
